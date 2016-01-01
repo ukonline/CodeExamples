81a2839f6252e1d0988ac98f63c661a9d2ad6466
Sébastien Combéfis <seb478@gmail.com>
2016-01-01 15:54:37 +0100

    Add skeletons files for Python exercises

---
+++ python/Exercises/bestdeal/skeletons/bestdeal.py
@@ -0,0 +1,12 @@
+# bestdeal.py
+
+def bestdeal(items, order):
+    # À compléter
+
+items = [
+    ["Clémentine", 0.5, 0.4, 0.2],
+    ["Sapin Nordmann 80cm", 25, 22, 18],
+    ["Foie gras 100g", 12.5, 10, 7]
+]
+order = [15, 10, 2]
+print(bestdeal(items, order))
---
+++ python/Exercises/bestmeal/skeletons/bestmeal.py
@@ -0,0 +1,12 @@
+# bestmeal.py
+
+def stats(votes):
+    # À compléter
+
+votes = {
+    "Pavé de biche aux airelles": {"Clémence": 9, "Quentin": 7, "Cédric": 2},
+    "Paupiette de Seitan": {"Clémence": 4, "Quentin": 6},
+    "Foie gras aux St Jacques": {"Quentin": 8, "Cédric": 1},
+    "Dinde de Noël farcie aux pommes et noix": {"Quentin": 9, "Cédric": 4}
+}
+print(stats(votes))
