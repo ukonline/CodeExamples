6b244afb014643647784e2e54f86994b0739c20c
Sébastien Combéfis <seb478@gmail.com>
2016-01-01 15:12:44 +0100

    Add code skeleton for Java exercises

---
+++ java/Exercises/palindrome/skeletons/Palindrome.java
@@ -0,0 +1,29 @@
+// Palindrome.java
+
+/**
+ * Programme qui teste si une chaine de caractères est un palindrome
+ * 
+ * @author Sébastien Combéfis
+ * @version January 1, 2016
+ */
+public class Palindrome
+{
+	private static String clean (String s)
+	{
+		// À compléter
+	}
+	
+	public static boolean isPalindrome (String s)
+	{
+		// À compléter
+	}
+	
+	public static void main (String[] args)
+	{
+		String str = "engage le jeu que je le gagne";
+		if (isPalindrome (str))
+		{
+			System.out.println ("'" + str + "' est un palindrome.");
+		}
+	}
+}
