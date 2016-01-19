538e1602490e960274bc2680f64756ac486ea5ef
Sébastien Combéfis <seb478@gmail.com>
2016-01-19 12:02:09 +0100

    Fix style of solution/Palindrome.java
    - Braces have to follow the Allman style
    - Spaces have been added for method calls, between operators in expressions
    - Spaces have been replaced by tabulations for indentation

--- java/Exercises/palindrome/solution/Palindrome.java
+++ java/Exercises/palindrome/solution/Palindrome.java
@@ -1,37 +1,46 @@
+// Palindrome.java
 
 /**
- * Write a description of class Serie1_Ex1 here.
+ * Programme qui teste si une chaine de caractères est un palindrome
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Louis Bokiau
+ * @author Sébastien Combéfis
+ * @version January 19, 2016
  */
 public class Palindrome
 {
-    public static void main (String[] args){
-        String str = "engage le jeu que je le gagne";
-        if (isPalindrome (str)){
-            System.out.println (str + " est un palindrome");
-        }
-    }
-
-    public static String clean(String s){
-        String clean = "";
-        for (int i=0; i<s.length(); i++){
-            if (s.charAt(i)!=' '){
-                clean+=s.charAt(i);
-            }
-        }
-        return clean;
-    }
-
-    public static boolean isPalindrome (String s){
-        String d = clean(s);
-        for (int i=0; i<=d.length()-i; i++){
-            if(d.charAt(i)!=d.charAt(d.length()-i)){
-                return false;
-            }
-        }
-        return true;
-    }
+	private static String clean (String s)
+	{
+		String clean = "";
+		for (int i = 0; i < s.length(); i++)
+		{
+			if (s.charAt (i) != ' ')
+			{
+				clean += s.charAt (i);
+			}
+		}
+		return clean;
+	}
+	
+	public static boolean isPalindrome (String s)
+	{
+		String d = clean (s);
+		for (int i = 0; i <= d.length() - i; i++)
+		{
+			if (d.charAt (i) != d.charAt (d.length() - i))
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static void main (String[] args)
+	{
+		String str = "engage le jeu que je le gagne";
+		if (isPalindrome (str))
+		{
+			System.out.println ("'" + str + "' est un palindrome.");
+		}
+	}
 }
-
