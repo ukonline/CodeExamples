f1c2953f076452039df7225451969cbb951c377d
Sébastien Combéfis <seb478@gmail.com>
2016-01-20 19:10:26 +0100

    Add "FileSum" Java exercise

---
+++ java/Exercises/filesum/skeletons/FileSum.java
@@ -0,0 +1,20 @@
+// FileSum.java
+
+/**
+ * Programme qui effectue des sommes de nombres entiers à partir de fichiers
+ * 
+ * @author Sébastien Combéfis
+ * @version January 20, 2016
+ */
+public class FileSum
+{
+	private static void computeSum (String inFile, String outFile)
+	{
+		// À compléter
+	}
+	
+	public static void main (String[] args)
+	{
+		computeSum ("input.txt", "output.txt");
+	}
+}
